e87bbe6d2d4b8cfe837baf530c8f3b39fc297f55
KowsiKalyan <kowsalyacherritech@gmail.com>
2022-11-11 12:25:53 +0530

    Update main.dart
    c

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _scanBarcode = 'Unknown';
+  String _scanBarcode1 = 'Unknown';
   TextEditingController text = TextEditingController();
 
   @override
